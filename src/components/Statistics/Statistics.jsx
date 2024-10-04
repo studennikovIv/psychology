41c1c4466101c185ac57b8ebba03141047188bfb
grant956 <68757554+studennikovIv@users.noreply.github.com>
2024-10-04 22:32:37 +0300

    1

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,30 @@
+import styles from './statistics.module.css';
+import PropTypes from 'prop-types';
+
+const Statistics = ({ title, stats }) => (
+  <section className={styles.statistics}>
+    {title && <h2 className={styles.title}>{title}</h2>}
+
+    <ul className={styles.stat_list}>
+      {stats.map(stat => (
+        <li className={styles.item} key={stat.id}>
+          <span className={styles.label}>{stat.label}</span>
+          <span className={styles.percentage}>{stat.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Statistics.prototype = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Statistics;
